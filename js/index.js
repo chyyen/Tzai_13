a5214e549e42662379242a5a8ddc9701a788c1c4
jonason2004 <jonason2004@gmail.com>
2023-08-24 00:26:52 +0800

    send picture function completed

--- js/index.js
+++ js/index.js
@@ -1,23 +1,46 @@
 let current_vertex = 0;
-function send_message(text, is_user){
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function send_message(text, is_user, sleep_time = 0){
     let chat_box = document.createElement("div");
-    if(is_user){
+    if (is_user) {
         chat_box.className = "flex w-full mt-2 mb-8 space-x-5 justify-end";
         chat_box.innerHTML = "<div class=\"w-fit\">\n" +
             "                    <div class=\"bg-blue-600 text-white p-3 rounded-full\">\n" +
-            `                        <p class="m-3 text-5xl">${text}</p>\n` +
+            `                        <p class="m-3 text-4xl">${text}</p>\n` +
             "                    </div>\n" +
-            "                </div>\n" +
-            "                <div class=\"flex-shrink-0 h-1/6 w-10 rounded-full bg-gray-300\"></div>"
+            "                </div>\n"
     }
     else{
-        chat_box.className = "flex w-full mt-2 mb-8 space-x-3";
-        chat_box.innerHTML = "<div class=\"flex-shrink-0 h-10 w-10 rounded-full bg-gray-300\"></div>\n" +
-            "                <div class=\"max-w-[80%] w-fit\">\n" +
-            "                    <div class=\"bg-gray-300 p-3 rounded-full\">\n" +
-            `                        <p class=\"m-3 text-5xl\">${text}</p>\n` +
-            "                    </div>\n" +
-            "                </div>"
+        sleep(sleep_time).then(() => {
+            chat_box.className = "flex w-full mt-2 mb-8 space-x-3";
+            chat_box.innerHTML = "<div class=\"flex-shrink-0 h-10 w-10 rounded-full bg-gray-300\"></div>\n" +
+                "                <div class=\"max-w-[80%] w-fit\">\n" +
+                "                    <div class=\"bg-gray-300 p-3 rounded-full\">\n" +
+                `                        <p class=\"m-3 text-4xl\">${text}</p>\n` +
+                "                    </div>\n" +
+                "                </div>"
+        });
+    }
+    document.getElementById("chats").appendChild(chat_box);
+}
+
+function send_photo(photo_path, is_user, sleep_time = 0){
+    let chat_box = document.createElement("div");
+    if (is_user) {
+
+    }
+    else{
+        sleep(sleep_time).then(() => {
+            chat_box.className = "flex w-full mt-2 mb-8 space-x-3";
+            chat_box.innerHTML = "<div class=\"flex-shrink-0 h-10 w-10 rounded-full bg-gray-300\"></div>\n" +
+                "                <div class=\"max-w-[80%] w-fit\">\n" +
+                `                     <img class="rounded-lg" src="${photo_path}">` +
+                "                </div>"
+        });
     }
     document.getElementById("chats").appendChild(chat_box);
 }
@@ -27,11 +50,18 @@ function change_state(e, sent_text){
         sent_text = "這題的答案是 " + sent_text;
     }
     send_message(sent_text, 1);
+    let sleep_time = reply_scripts[e].sleep_time;
     // reply
     let reply = reply_scripts[e];
     for(let i = 0; i < reply.text.length; i++){
         let text = reply.text[i];
-        send_message(text, 0);
+        send_message(text, 0, sleep_time);
+    }
+    // photo
+    sleep(1000).then(() => {});
+    let photos = reply.photos;
+    for(let i = 0; i < photos.length; i++){
+        send_photo(`images/${photos[i]}`, 0, sleep_time);
     }
     // change state
     current_vertex = reply.next_vertex;
--- js/script.js
+++ js/script.js
@@ -55,41 +55,48 @@ reply_scripts = [
     {
         text: ["你能教英文嗎？需要家教"],
         photos: [],
-        next_vertex : 1
+        next_vertex : 1,
+        sleep_time : 600
     },
     {
         text : ["那你可以幫我解答這些題目嗎？"],
         photos: [],
-        next_vertex : 2
+        next_vertex : 2,
+        sleep_time : 400
     },
     {
         text : ["我很認真啦！！！！",
             "還是你可以幫我解題目就好 🥺"],
         photos: [],
-        next_vertex : 3
+        next_vertex : 3,
+        sleep_time : 400
     },
     {
         text : ["不要鬧啦 🥹 幫我解題我就告訴你巨龍的線索"],
         photos: [],
-        next_vertex : 3
+        next_vertex : 3,
+        sleep_time : 400
     },
     {
         text : [],
         photos: ["problem.jpg"],
-        next_vertex : 4
+        next_vertex : 4,
+        sleep_time : 400
     },
     {
         text : ["謝謝你！！這些就當報酬吧"],
         photos : ["image83.jpg",
             "image2.jpg",
             "bus_stop.jpg"],
-        next_vertex : -1
+        next_vertex : -1,
+        sleep_time : 400
     },
     {
-        text : ["欸我看解答好像不是這樣⋯", "你好像不夠努力 🥹 但還是謝謝"],
+        text : ["欸我看解答好像不是這樣⋯", "你好像有點笨 🥹，但看在你這麼努力的份上還是給你線索吧"],
         photos : ["image83.jpg",
             "image2.jpg",
             "bus_stop.jpg"],
-        next_vertex : -1
+        next_vertex : -1,
+        sleep_time : 400
     }
 ]
